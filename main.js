5a977c20b08b7b3968e71062c5e0130f6c554a1d
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-08 20:26:26 -0500

    Se añadió el fondo al body y la disposicion de los elementosjuego e instrucciones

--- main.js
+++ main.js
@@ -191,14 +191,14 @@
       switch(element.kind){
         case "rectangle":
           ctx.fillRect(element.x,element.y,element.width,element.height);
-          ctx.fillStyle = '#FF0000';
+          ctx.fillStyle = '#009900';
           break;
         case "circle":
           ctx.beginPath();
           ctx.arc(element.x,element.y,element.radius,0,7);
           ctx.fill();
           ctx.closePath();
-          ctx.fillStyle = '#FF0000';
+          ctx.fillStyle = '#009900';
           break;
       }
     }
